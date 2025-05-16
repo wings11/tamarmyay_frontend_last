5c97da69879f83fd0e14ce6d5cf29bd443cc9696
WaiTgC <waiyanlintg@gmail.com>
2025-05-16 20:45:25 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import HomePage from './pages/HomePage';
-import CreateOrder from './components/CreateOrder';
-import Checkout from './components/Checkout';
-import OrderHistory from './components/OrderHistory';
-import SalesReport from './components/SalesReport';
-import ManageItems from './components/ManageItems';
-import ManageLocations from './components/ManageLocations';
-import './styles.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import HomePage from "./pages/HomePage";
+import CreateOrder from "./components/CreateOrder";
+import Checkout from "./components/Checkout";
+import OrderHistory from "./components/OrderHistory";
+import SalesReport from "./components/SalesReport";
+import ManageItems from "./components/ManageItems";
+import ManageLocations from "./components/ManageLocations";
+import "./styles.css";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   const handleLogin = (newToken) => {
     setToken(newToken);
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
   };
 
   const handleLogout = () => {
     setToken(null);
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
   };
 
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={token ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
-        <Route path="/" element={token ? <HomePage onLogout={handleLogout} /> : <Navigate to="/login" />}>
+        <Route
+          path="/login"
+          element={
+            token ? <Navigate to="/" /> : <Login onLogin={handleLogin} />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            token ? (
+              <HomePage onLogout={handleLogout} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        >
           <Route index element={<CreateOrder token={token} />} />
           <Route path="checkout" element={<Checkout token={token} />} />
           <Route path="history" element={<OrderHistory token={token} />} />
@@ -37,9 +56,6 @@ function App() {
         </Route>
       </Routes>
     </Router>
-
-
-
   );
 }
 
